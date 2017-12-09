5d406566efac0260bc43c0bc0b9deba52c45761c
Joanna Skorupa <joanna.skorupa@ready4s.pl>
2017-12-09 19:05:16 +0100

    Showing photos in product detail

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -12,13 +12,25 @@ import { Product } from '../product';
 export class ProductDetailsComponent implements OnInit {
 
   product: Product;
+  photos: string[];
+  mainPhoto: string;
+  p: number = 1;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
     private router: Router,
     private location: Location
-  ) { }
+  ) {
+    this.photos = [ 
+      "https://4.allegroimg.com/s512/01e9ca/90a56ebe470e9158e52a4023b934",
+      "https://5.allegroimg.com/s512/0310ef/0919d53f40b98854a02c7fb7dd95",
+      "https://2.allegroimg.com/original/01b5b6/cbf769a549f9aadfb10ac748efd2",
+      "https://7.allegroimg.com/original/01e837/fdec88a34093baa1de0eccb488b7",
+      "https://c.allegroimg.com/original/03c98d/307b6196494dbd67107c26ae5afc"
+    ];
+    this.mainPhoto = this.photos[0];
+  }
 
   ngOnInit() {
     var id: string;
@@ -27,6 +39,10 @@ export class ProductDetailsComponent implements OnInit {
     });
     this.product = new Product("", "", null, "", "", null, []);
     this.getProduct(id);
+  }
+
+  public onPrevClicked(photo: string) {
+    this.mainPhoto = photo;
   }
   
   public onBackButtonClicked() {
